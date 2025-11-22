83442acf306e320ad3e32887906103c166097659
Daksh Adhar <dakshadhar1810@gmail.com>
2025-11-22 00:53:43 -0500

    [WIP] Added rotation

--- proj5/eval_cls.py
+++ proj5/eval_cls.py
@@ -13,6 +13,7 @@ def create_parser():
     parser.add_argument('--num_cls_class', type=int, default=3, help='The number of classes')
     parser.add_argument('--num_points', type=int, default=10000, help='The number of points per object to be included in the input data')
     parser.add_argument('--batch_size', type=int, default=256, help='Batch size for evaluation')
+    parser.add_argument('--theta', type=int, default=0, help='Rotates gt point cloud by theta in deg')
 
     # Directories and checkpoint/sample iterations
     parser.add_argument('--load_checkpoint', type=str, default='model_epoch_0')
@@ -52,6 +53,16 @@ if __name__ == '__main__':
     test_label = torch.from_numpy(np.load(args.test_label))
 
     # ------ TO DO: Make Prediction ------
+    if args.theta:
+        theta = torch.tensor(torch.pi*args.theta/180)
+        c, s = torch.cos(theta), torch.sin(theta)
+        R = torch.eye(3)
+        R[1, 1] = c
+        R[1, 2] = -s
+        R[2, 1] = s
+        R[2, 2] = c
+        test_data = test_data @ R.T
+
     num_samples=test_data.shape[0]
     correct=0
     total=0
@@ -70,5 +81,9 @@ if __name__ == '__main__':
     test_accuracy = correct / total
     print ("test accuracy: {}".format(test_accuracy))
 
+    with torch.no_grad():
+        sample_data = test_data[args.i:args.i+1].to(args.device)
+        pred_label_sample = model(sample_data).argmax(-1).cpu()[0].item()
+    
     viz_cls(test_data[args.i], test_label[args.i].item(), "{}/gt_{}.gif".format(args.output_dir, args.exp_name), args.device)
-    viz_cls(test_data[args.i], pred_label[args.i].item(), "{}/pred_{}.gif".format(args.output_dir, args.exp_name), args.device)
+    viz_cls(test_data[args.i], pred_label_sample, "{}/pred_{}.gif".format(args.output_dir, args.exp_name), args.device)
